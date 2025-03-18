8ba1ebfd470c61ea9d12636d71df873490e888f7
Virtue Xolutions <kinzakhurram84@gmail.com>
2025-03-18 15:27:50 +0500

    changes

--- SRC/Screens/MapScreen.js
+++ SRC/Screens/MapScreen.js
@@ -1,7 +1,7 @@
-import {useIsFocused, useNavigation} from '@react-navigation/native';
-import {isValidCoordinate} from 'geolib';
-import {Icon} from 'native-base';
-import React, {useEffect, useRef, useState} from 'react';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import { isValidCoordinate } from 'geolib';
+import { Icon } from 'native-base';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   ActivityIndicator,
   Alert,
@@ -10,18 +10,18 @@ import {
   View,
 } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import Pulse from 'react-native-pulse';
-import {moderateScale} from 'react-native-size-matters';
+import { moderateScale } from 'react-native-size-matters';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import Color from '../Assets/Utilities/Color';
-import {Get, Post} from '../Axios/AxiosInterceptorFunction';
+import { Get, Post } from '../Axios/AxiosInterceptorFunction';
 import DeclineModal from '../Components/DeclineModal';
 import RequestModal from '../Components/RequestModal';
 import navigationService from '../navigationService';
-import {apiHeader, windowHeight, windowWidth} from '../Utillity/utils';
+import { apiHeader, windowHeight, windowWidth } from '../Utillity/utils';
 import CustomButton from '../Components/CustomButton';
 import database, {
   firebase,
@@ -41,7 +41,7 @@ const MapScreen = props => {
   const paymentMethod = props?.route?.params?.paymentMethod;
   const nearestcab = props?.route?.params?.isEnabled;
   const fromrideScreen = props?.route?.params?.fromrideScreen;
-  const {user_type} = useSelector(state => state.authReducer);
+  const { user_type } = useSelector(state => state.authReducer);
   console.log('🚀 ~ user_type:', user_type);
 
   const token = useSelector(state => state.authReducer.token);
@@ -160,9 +160,11 @@ const MapScreen = props => {
     for (let key in body) {
       formData.append(key, body[key]);
     }
+    console.log("🚀 ~ requestforRide ~ body:", formData)
     const url = 'auth/bookride';
     setIsLoading(true);
     const response = await Post(url, formData, apiHeader(token));
+    console.log("🚀 ~ requestforRide ~ response:", response?.data)
     setIsLoading(false);
     if (response != undefined) {
       setRideID(response?.data.data?.id);
@@ -199,16 +201,16 @@ const MapScreen = props => {
           latitudeDelta: 0.0522,
           longitudeDelta: 0.0521,
         }}
-        // initialCamera={{
-        //   center: {
-        //     latitude: currentPosition?.latitude || 0,
-        //     longitude: currentPosition?.longitude || 0,
-        //   },
-        //   pitch: 0,
-        //   zoom: 18,
-        //   heading: 0,
-        //   altitude: 1000,
-        // }}
+      // initialCamera={{
+      //   center: {
+      //     latitude: currentPosition?.latitude || 0,
+      //     longitude: currentPosition?.longitude || 0,
+      //   },
+      //   pitch: 0,
+      //   zoom: 18,
+      //   heading: 0,
+      //   altitude: 1000,
+      // }}
       ></MapView>
 
       <Pulse
@@ -228,10 +230,10 @@ const MapScreen = props => {
           as={FontAwesome5}
           size={moderateScale(30, 0.6)}
           color={Color.white}
-          style={{left: 5}}
+          style={{ left: 5 }}
         />
       </View>
-      <View style={{position: 'absolute', bottom: 20}}>
+      <View style={{ position: 'absolute', bottom: 20 }}>
         {/* <AskLocation
           main_view_style={{height: windowHeight * 0.17}}
           heading={'Waiting For Replies'}
